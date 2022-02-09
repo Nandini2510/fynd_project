5a12639ad6299bea8709f1a94ca886f3b530d1ba
Nandini2510 <nandini976804@gmail.com>
2022-02-09 18:44:12 +0530

    javascript data types

---
+++ portfolio/script.js
@@ -0,0 +1,17 @@
+var my_name = "Nandini";
+var age = 21;
+var firstTimeJs = false;
+var my_char = "n";
+var my_check;
+var my_float = 9.2;
+var exponent = 2333998973993992n;
+var my_obj = {};
+var my_sym = Symbol("this is a symbol");
+var my_test = null;
+
+console.log(my_name);
+console.log(age);
+console.log(firstTimeJs);
+console.log(typeof my_test);
+console.log(parseInt("Joker")); //Nan
+//undefined value to string is nan
